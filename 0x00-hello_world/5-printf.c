90314407273d914ef8f02a10be89c510080fd420
mohamed81443 <mohamed.ibrahim81443@gmail.com>
2023-02-18 01:59:16 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - printf
+ *
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
