dfaf219fe07a16d29af9fd7c28bfaeb8ec6154db
mohamed81443 <mohamed.ibrahim81443@gmail.com>
2023-02-26 06:45:14 -0800

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks digit
+ * @c: number
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	int i;
+
+	for (i = '0'; i <= 9; i++)
+	{
+		if (i == c)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
