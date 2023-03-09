7835986c7aaa3418dd025332de2bbfd3678bd9fa
mohamed81443 <mohamed.ibrahim81443@gmail.com>
2023-03-09 01:49:55 -0800

    task 0

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * helperfunction - func
+ * @num: number
+ * @i: possible
+ *
+ * Return: 0 or 1
+ */
+int helperfunction(int num, int i)
+{
+	if (i < num)
+	{
+		if (num % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (helperfunction(num, i + 1));
+		}
+	}
+	else
+	{
+		return (1);
+	}
+}
+/**
+ * is_prime_number - func
+ * @n: number
+ *
+ * Return: 1
+ * 0 not.
+ */
+int is_prime_number(int n)
+{
+	if (n <= i)
+	{
+		return (0);
+	}
+	else
+	{
+		return (helperfunction(n, 2));
+	}
+}
