f05de11ce8a538101a02f0ca16d6fde7c605b8d8
mohamed81443 <mohamed.ibrahim81443@gmail.com>
2023-03-09 02:59:59 -0800

    task 6

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -35,13 +35,13 @@ int _strlen_recursion(char *s)
  * @i: iter
  * @len: leng
  *
- * Return: 1
+ * Return: 1 if palindroom, 0 if not
  */
 int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
 		return (0);
 	if (i >= len)
-		return (1)
+		return (1);
 			return (check_pal(s, i + 1, len - 1));
 }
