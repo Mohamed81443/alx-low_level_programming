a78455e809a78a9b8289aa093aa2c2b47bfee142
mohamed81443 <mohamed.ibrahim81443@gmail.com>
2023-03-09 02:47:26 -0800

    task 6

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -17,6 +17,19 @@ int is_palindrome(char *s)
 }
 
 /**
+ * _strlen_recursion - func
+ * @s: string
+ *
+ * Return: leng
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\n'
+			return (0);
+			return (1 + _strlen_recursion(s + 1));
+}
+
+/**
  * check_pal - func
  * @s: str
  * @i: iter
